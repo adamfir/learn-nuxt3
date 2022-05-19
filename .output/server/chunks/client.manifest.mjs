84ad48f3e573136536c9db7c4f4bb0744775760e
Adam Firdaus <adamfir@pahamify.com>
2022-05-20 00:26:58 +0700

    feat: exclude output

---
+++ .output/server/chunks/client.manifest.mjs
@@ -0,0 +1,13 @@
+const client_manifest = {
+  "node_modules/nuxt/dist/app/entry.mjs": {
+    "file": "entry-e6da15fc.mjs",
+    "src": "node_modules/nuxt/dist/app/entry.mjs",
+    "isEntry": true,
+    "css": [
+      "entry.fec640a4.css"
+    ]
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
